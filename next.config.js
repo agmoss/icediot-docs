4df222b87f2e950107d6e4f5d690bda3dc77f3ca
Andrew Moss <andrew@m0ss.dev>
2022-04-15 12:54:58 -0600

    chore: Package upgrade, altering some configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack5: true,
+  productionBrowserSourceMaps: false,
+  images: {
+      formats: ["image/avif", "image/webp"],
+  },
+  experimental: {
+      outputStandalone: true,
+  },
 };
